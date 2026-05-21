fdf59e232be4d127245ea194bbc23944755367e1
Fth0606 <Fthalmh0@gmail.com>
2026-05-21 20:15:18 +0100

    Revert "Integrate Super Admin Interface with Backend"

--- backend/app/Models/Plan.php
+++ backend/app/Models/Plan.php
@@ -6,10 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    protected $fillable = ['name', 'slug', 'description', 'price_monthly', 'price_yearly', 'max_users', 'features', 'published'];
-
-    protected $casts = [
-        'features' => 'array',
-        'published' => 'boolean',
-    ];
+    //
 }
